44b9211d2dbe976f7214c08c37e6b11104545a05
vchernogorov <vchernogorov@griddynamics.com>
2016-11-05 21:07:39 +0300

    Create component directory with one test component

--- app/app.js
+++ app/app.js
@@ -1,22 +1,11 @@
 'use strict';
 
 angular.module('app', ['ngRoute'])
-  .config(['$routeProvider',
-    function config($routeProvider) {
-      $routeProvider
-        .when('/page1', { templateUrl: 'page1.html', controller: 'Page1Ctrl' })
-        .when('/page2', { templateUrl: 'page2.html', controller: 'Page2Ctrl' })
-        .otherwise('/');
-    }
-  ])
-  .controller('HelloCtrl', function HelloCtrl($scope) {
-    $scope.yourName = 'LOL'
-  })
-  .controller('Page1Ctrl', ['$routeParams',
-  function Page1Ctrl($routeParams) {
+  .controller('LoginCtrl', ['$routeParams',
+  function LoginCtrl($routeParams) {
     this.$routeParams = $routeParams;
   }])
-  .controller('Page2Ctrl', ['$routeParams',
-  function Page2Ctrl($routeParams) {
+  .controller('CoursesCtrl', ['$routeParams',
+  function CoursesCtrl($routeParams) {
     this.$routeParams = $routeParams;
   }]);
