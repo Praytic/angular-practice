507377e8a25c0d8904060e824b23e82782aa6ad0
vchernogorov <vchernogorov@griddynamics.com>
2016-11-05 18:47:45 +0300

    Add stubbed pages and create ng-view

--- app/app.js
+++ app/app.js
@@ -1,5 +1,22 @@
-var angularPracticeApp = angular.module('app', []);
+'use strict';
 
-angularPracticeApp.controller('HelloCtrl', function HelloCtrl($scope) {
+angular.module('app', ['ngRoute'])
+  .config(['$routeProvider',
+    function config($routeProvider) {
+      $routeProvider
+        .when('/page1', { templateUrl: 'page1.html', controller: 'Page1Ctrl' })
+        .when('/page2', { templateUrl: 'page2.html', controller: 'Page2Ctrl' })
+        .otherwise('/');
+    }
+  ])
+  .controller('HelloCtrl', function HelloCtrl($scope) {
     $scope.yourName = 'LOL'
-});
+  })
+  .controller('Page1Ctrl', ['$routeParams',
+  function Page1Ctrl($routeParams) {
+    this.$routeParams = $routeParams;
+  }])
+  .controller('Page2Ctrl', ['$routeParams',
+  function Page2Ctrl($routeParams) {
+    this.$routeParams = $routeParams;
+  }]);
