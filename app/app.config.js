084af198a0e50ef55f3420615488f88bde9373f7
vchernogorov <vchernogorov@griddynamics.com>
2016-11-06 22:55:14 +0300

    Create Course factory and add jsons

--- app/app.config.js
+++ app/app.config.js
@@ -3,7 +3,7 @@ angular.module('app').config(['$routeProvider',
     $routeProvider
       .when('/login', { templateUrl: 'login.html', controller: 'LoginCtrl' })
       .when('/course', { templateUrl: 'courses.html', controller: 'CoursesCtrl' })
-      .when('/course/:courseName', { templateUrl: 'courseDetail.html', controller: 'CourseDetailCtrl' })
+      .when('/course/:courseId', { templateUrl: 'component/courseDetail.html', controller: 'CourseDetailCtrl' })
       .otherwise('/');
   }
 ]);
--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('app', ['ngRoute']);
+angular.module('app', ['ngRoute', 'ngResource']);
